152e1b45c24e7277e9cf08e0b18993793a2a6adc
hlainn <hlainnpoeei@gmail.com>
2021-03-09 19:48:48 +0630

    First next js blog

---
+++ pages/api/hello.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' })
+    
+    //const email = req.body.email
+    // Then save email to your database, etc...
+  }
